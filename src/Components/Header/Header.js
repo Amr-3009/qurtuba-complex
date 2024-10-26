31d01c25fc4ba90eedbeae5b5ab9faddf1ebda69
Amr-3009 <amrosama1k@gmail.com>
2024-10-26 13:10:19 +0300

    Add initial project files and structure

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h3>
+        Tarbyh Namouthajiyah Schools in Alnarjis & Qurtoba are now open{" "}
+        <a href="https://ncle-sms.com/Admission" target="blank">
+          Register Now
+        </a>
+      </h3>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/assets/SVG/LangSwitch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LangSwitch = () => {
+  return <></>;
+};
+
+export default LangSwitch;

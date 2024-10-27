1d782a2168a17af0c66be6bea646cff0a499e7bb
Amr-3009 <amrosama1k@gmail.com>
2024-10-27 21:16:23 +0300

    Refactor ParentMeeting1Img.js: Add ParentMeetingPhotos array with one photo

---
+++ src/ParentMeeting1Img.js
@@ -0,0 +1,86 @@
+export const ParentMeetingPhotos = [
+  {
+    src: "/assets/images/parent-meeting/1.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/2.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/3.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/4.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/5.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/6.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/7.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/8.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/9.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/10.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/11.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/12.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/13.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/14.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/15.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/16.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/17.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/18.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/19.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/20.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/21.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/22.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/23.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/24.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/25.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/26.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/27.png"
+  },
+  {
+    src: "/assets/images/parent-meeting/28.png"
+  }
+]
